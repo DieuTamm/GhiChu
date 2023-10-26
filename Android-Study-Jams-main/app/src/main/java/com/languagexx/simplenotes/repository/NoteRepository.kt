c27e44c74bb80ab96fd1a7400eb2674ab49db07f
DieuTamm <142554569+DieuTamm@users.noreply.github.com>
2023-10-26 13:35:32 +0700

    gki

---
+++ Android-Study-Jams-main/app/src/main/java/com/languagexx/simplenotes/repository/NoteRepository.kt
@@ -0,0 +1,33 @@
+package com.languagexx.simplenotes.repository
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.languagexx.simplenotes.persistence.NoteDao
+import com.languagexx.simplenotes.persistence.Note
+import kotlinx.coroutines.*
+import javax.inject.Inject
+class NoteRepository @Inject constructor(val noteDao: NoteDao) {
+    fun insert(note: Note) {
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.insert(note)
+        }
+    }
+    fun delete(note: Note) {
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.delete(note)
+        }
+    }
+    fun deleteById(id: Int) {
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.deleteById(id)
+        }
+    }
+    fun update(note: Note) {
+        CoroutineScope(Dispatchers.IO).launch {
+            noteDao.update(note)
+            Log.e("DEBUG", "update is called in repo")
+        }
+    }
+    fun getAllNotes(): LiveData<List<Note>>{
+        return noteDao.getAllNotes()
+    }
+}
